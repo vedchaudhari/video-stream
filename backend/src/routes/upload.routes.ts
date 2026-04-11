1c9060ac4b931c1f2dd5190a2b82acf656a2b300
vedchaudhari <vedc2853@gmail.com>
2026-04-12 02:14:15 +0530

    Remove website package.json and video player component files

--- backend/src/routes/upload.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import { uploadController } from "../controllers/upload.controllers.js";
-import { upload } from "../middleware/upload.middleware.js";
-
-const router = Router();
-
-router.post("/upload",upload.single("video-file"), uploadController.uploadVideo);
-
-export default router;
